f3552be22e40438af5f781af40a34b919f40cbf3
fujitaiori <ioriori0413@gmail.com>
2022-10-28 11:46:00 +0900

    import先を修正

--- ビジュアルリグレッションテストを書こう/my-app/src/index.js
+++ ビジュアルリグレッションテストを書こう/my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Game } from './Game';
+import { Game } from './game/Game';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
